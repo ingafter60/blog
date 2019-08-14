db4d2cd7a31f592bb33336780ba369bbe405537a
ing <inyoman_gurnitha@yahoo.com>
2019-08-14 21:10:43 +0700

    Ch5: Blog app

---
+++ blog/views.py
@@ -0,0 +1,10 @@
+# blog/views.py
+
+from django.views.generic import ListView
+from .models import Post
+
+# Create your views here.
+
+class BlogListView(ListView):
+	model 				= Post
+	template_name = 'home.html'
